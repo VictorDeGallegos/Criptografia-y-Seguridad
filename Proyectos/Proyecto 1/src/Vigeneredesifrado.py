ab4b87101c861baa0e2f8580c8154ee8ec1a1351
Demian <53890358+demian35@users.noreply.github.com>
2022-09-19 11:47:27 -0500

    Documente el script de Vigeneredesifrado.py

--- Proyectos/Proyecto 1/src/Vigeneredesifrado.py
+++ Proyectos/Proyecto 1/src/Vigeneredesifrado.py
@@ -1,13 +1,20 @@
 ##Practica 1
 ##Desifrado de vigenere
+##Integrantes de Equipo 
+##Jose Demian Jimenez Salgado
+##Carlos Cruz Rangel
+##Victor Hugo Gallegos Mota
 
+##Clave de Vigenere
 clave='LATIN'
 
+##Funcion para llevar a cabo el ataque a vigenere
 def crackeaVinegere(x, k):
     ind1 = ord(x)-65
     ind2 = ord(k)-65
     return (ind1-ind2)%26
 
+##funcion para leer un archivo de texto el cual se va a desencriptar
 def desencripta(archivo):
     file = open(archivo,'r')
     texto = file.readlines()
