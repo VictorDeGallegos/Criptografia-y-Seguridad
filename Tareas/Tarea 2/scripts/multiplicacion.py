5be481d7233a60c57c13ee18e1f4e7d18626c43d
Victor Gallegos <316160456@ciencias.unam.mx>
2022-11-05 09:27:36 -0600

    Agrega Script multiplicacion.py
    Agrega script para encontrar 2 numeros que nos den la llave

---
+++ Tareas/Tarea 2/scripts/multiplicacion.py
@@ -0,0 +1,14 @@
+# Script para encontrar 2 numeros que multiplciados me den un numero n
+
+def multiplicacion():
+    n = 7784099
+    x = 2
+    while n != 1:  # Mientras n sea diferente de 1
+        if n % x == 0:  # Si el residuo de n entre x es 0
+            print(x)  # Imprime x
+            n = n/x  # n es igual a n entre x
+        else:
+            x = x+1  # Si no x es igual a x mas 1
+
+
+multiplicacion()
